7e84b39c9e91724785d74c78421c744a9fc30334
ryowuandjanet <ryowudata@gmail.com>
2020-05-13 23:05:04 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+	constructor(){
+		super();
+		this.state={displayBio:true};
+	}
+
+	render(){
+		const bio=this.state.displayBio ? (
+			<div>
+				<p>I am ryowu.</p>
+				<p>43 year old.</p>
+				<p>Live in Tainan.</p>
+			</div>
+		) : null;
+
+		return (
+			<div>
+				<h1>Hello!</h1>
+				<p>My name is David. I am a software engineer.</p>
+				<p>I am always looking forward to working on meaningful projects.</p>
+				<div>
+					<p>I live in San Francisco, and code every day.</p>
+					<p>My favorite language is Javascript</p>
+					<p>Besides coding, I also love music and ramen!</p>
+
+					{
+						this.state.displayBio ? (
+							<div>
+								<p>I am ryowu.吳小毛</p>
+								<p>43 year old.</p>
+								<p>Live in Tainan.</p>
+							</div>
+						) : null
+					}
+				</div>
+			</div>
+		)
+	}
 }
+
 
 export default App;
